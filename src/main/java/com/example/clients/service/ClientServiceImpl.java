15bec2f9776fc39c00ce0df4f610b25dc48fc58b
GeorgyVShefer <156106194+GeorgyVShefer@users.noreply.github.com>
2025-02-10 20:26:35 +0300

    TASK_0001 Создать метод save для сохранения нового клиента

--- src/main/java/com/example/clients/service/ClientServiceImpl.java
+++ src/main/java/com/example/clients/service/ClientServiceImpl.java
@@ -19,4 +19,8 @@ public class ClientServiceImpl implements ClientService {
     public Client  getById(Long id){
         return repository.findById(id).orElse(null);
     }
+
+    public Client create(Client client){
+        return repository.save(client);
+    }
 }
