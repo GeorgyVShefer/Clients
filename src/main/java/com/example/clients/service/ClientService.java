320253617360e6a685e50805a643494c068bfac8
GeorgyVShefer <156106194+GeorgyVShefer@users.noreply.github.com>
2025-02-10 20:06:24 +0300

    Initial commit

---
+++ src/main/java/com/example/clients/service/ClientService.java
@@ -0,0 +1,4 @@
+package com.example.clients.service;
+
+public interface ClientService {
+}
---
+++ src/main/java/com/example/clients/service/ClientServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.clients.service;
+
+import com.example.clients.model.Client;
+import com.example.clients.repository.ClientRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class ClientServiceImpl implements ClientService {
+    ClientRepository repository;
+
+    public List<Client> getAll(){
+        return repository.findAll();
+    }
+
+    public Client  getById(Long id){
+        return repository.findById(id).orElse(null);
+    }
+}
